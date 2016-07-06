a872f5b40a6c2d71cb2e2c2abefd4b23c21d1fdc
CatLogic <yixianxueqi@163.com>
2016-07-06 15:17:41 +0800

    init project

---
+++ YHDevelopFramework/YHDevelopFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHDevelopFramework
+//
+//  Created by 君若见故 on 16/7/6.
+//  Copyright © 2016年 isoftstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHDevelopFramework/YHDevelopFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHDevelopFramework
+//
+//  Created by 君若见故 on 16/7/6.
+//  Copyright © 2016年 isoftstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
