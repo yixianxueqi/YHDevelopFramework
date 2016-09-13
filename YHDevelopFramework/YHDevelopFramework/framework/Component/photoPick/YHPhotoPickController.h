3abdd4722f64e5d3a4bd078abe6e9d3b86fddf66
CatLogic <yixianxueqi@163.com>
2016-09-13 10:36:51 +0800

    a stable version

---
+++ YHDevelopFramework/YHDevelopFramework/framework/Component/photoPick/YHPhotoPickController.h
@@ -0,0 +1,13 @@
+//
+//  YHPhotoPickController.h
+//  YHDevelopFramework
+//
+//  Created by 君若见故 on 16/9/13.
+//  Copyright © 2016年 isoftstone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YHPhotoPickController : NSObject
+
+@end
---
+++ YHDevelopFramework/YHDevelopFramework/framework/Component/photoPick/YHPhotoPickController.m
@@ -0,0 +1,17 @@
+//
+//  YHPhotoPickController.m
+//  YHDevelopFramework
+//
+//  Created by 君若见故 on 16/9/13.
+//  Copyright © 2016年 isoftstone. All rights reserved.
+//
+
+#import "YHPhotoPickController.h"
+
+@interface YHPhotoPickController ()
+
+@end
+
+@implementation YHPhotoPickController
+
+@end
